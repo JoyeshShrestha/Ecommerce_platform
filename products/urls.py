4fe52afee777c8f5a6c957b056fb429071820996
Joyesh Shrestha <sjoyesh2000@gmail.com>
2023-10-17 23:28:28 +0545

    Search Specific products by name

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import ItemListingViewSet,GetAllItemsView
+from .views import ItemListingViewSet,GetAllItemsView,SpecificItemsViewByName
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -8,6 +8,8 @@ router.register(r'', ItemListingViewSet)
 
 urlpatterns = [
     path('', GetAllItemsView.as_view()),
+    path('search/', SpecificItemsViewByName.as_view(),name='specific-items-by-name'),
+
     path('dashboard/', include(router.urls)),
     
 
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 # views.py
-
+from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework import viewsets
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
@@ -7,7 +7,7 @@ from django.http import JsonResponse
 from django.views import View
 from .models import Products
 from .serializers import ProductsSerializer
-
+from registration.models import User
 import jwt
 
 class ItemListingViewSet(viewsets.ModelViewSet):
@@ -23,6 +23,52 @@ class GetAllItemsView(View):
 
         # Return the serialized items as JSON response
         return JsonResponse(serializer.data, safe=False)
+    
+    
+        
+class SpecificItemsViewByName(View):
+    #search item by name
+    def post(self, request, *args, **kwargs):
+    
+        search_name = request.POST.get('name')
 
+        items = Products.objects.filter(name = search_name).first()
+
+
+        # Return the serialized items as JSON response
+        # return Response(items)
+        if items:
+            # Serialize the item using the ProductsSerializer
+            serializer = ProductsSerializer(items)
+            return JsonResponse(serializer.data, safe=False)
+        else:
+            # Handle the case when the item is not found
+            return JsonResponse({'message': 'Item not found'}, status=404)
+    def get(self, request, *args, **kwargs):
+        # Retrieve the JWT from cookies
+        jwt_token = request.COOKIES.get('jwt')
+
+        if jwt_token:
+            try:
+                # Decode the JWT to access user information
+                decoded_payload = jwt.decode(jwt_token, 'secret_key', algorithms=['HS256'])
+                user_id = decoded_payload['id']
+                
+                # Add your logic to retrieve and return user-specific data here
+                # For example:
+                user = User.objects.get(id=user_id)
+                user_data = {
+                    'id': user.id,
+                    'username': user.username,
+                    # Add more user data fields as needed
+                }
+                
+                return JsonResponse(user_data)
+            except jwt.ExpiredSignatureError:
+                return JsonResponse({'error': 'JWT has expired'}, status=401)
+            except jwt.DecodeError:
+                return JsonResponse({'error': 'Invalid JWT'}, status=401)
+        else:
+            return JsonResponse({'error': 'JWT not found'}, status=401)    
 
 
